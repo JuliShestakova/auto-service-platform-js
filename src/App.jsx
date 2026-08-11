cc264439a210831cb5d6dd29662fd20d70688477
JuliShestakova <265223466+JuliShestakova@users.noreply.github.com>
2026-08-11 15:53:22 +0300

    Add customer dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,22 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import CustomerDashboardPage from './pages/CustomerDashboardPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+
         <Route path="/login" element={<LoginPage />} />
+
         <Route path="/register" element={<RegisterPage />} />
+
+        <Route
+          path="/customer"
+          element={<CustomerDashboardPage />}
+        />
       </Routes>
     </BrowserRouter>
   );
