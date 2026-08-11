938616588917d207eed3ad2ebace2f76210f0f70
JuliShestakova <265223466+JuliShestakova@users.noreply.github.com>
2026-08-11 15:44:27 +0300

    Add login page

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+
+function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log('Вход:', {
+      email,
+      password,
+    });
+  };
+
+  return (
+    <main>
+      <h1>Вход</h1>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">
+            Email
+          </label>
+
+          <input
+            id="email"
+            type="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            placeholder="Введите email"
+            required
+          />
+        </div>
+
+        <div>
+          <label htmlFor="password">
+            Пароль
+          </label>
+
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            placeholder="Введите пароль"
+            required
+          />
+        </div>
+
+        <button type="submit">
+          Войти
+        </button>
+      </form>
+    </main>
+  );
+}
+
+export default LoginPage;
